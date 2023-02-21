404422d1b56502f92e146979e4b0f308808dc882
sjy <xiaoshengbusi@gmail.com>
2023-02-21 21:35:44 +0800

    no message

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello golang!",
+			"message": "Hello 我是golang啊!",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
